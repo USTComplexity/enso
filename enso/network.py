Add: corrected network calculation

enso/network.py
@@ -9,23 +9,44 @@ def corr_2pt_avg(x: npt.ArrayLike, f: npt.ArrayLike, lag_max: int, window: int):
     c = np.zeros([n_t - lag_max - 1 - window, n_x, n_y])
 
     for s in range(lag_max + 1):
-        c += corr_2pt(x, f, s, lag_max, window)[lag_max + 1 + window - s:,:,:]
+        c += corr_2pt(x, f, s, window)[lag_max + 1 + window - s:,:,:]
 
     c /= (lag_max + 1)
 
     print(np.mean(np.abs(c)))
 
+    # Sanity check
     if np.max(c) > 1 or np.min(c) < -1:
         raise ValueError("Something wrong in the correlation matrix")
 
     return c
 
 
-def corr_2pt(x: npt.ArrayLike, f: npt.ArrayLike, lag: int, lag_max: int, window: int):
+def str_2pt(x: npt.ArrayLike, f: npt.ArrayLike, lag_max: int, window: int):
+    
+    n_t, n_x, n_y = f.shape
+    c = np.zeros([n_t - lag_max - 1 - window, n_x, n_y])
+    c2 = np.zeros([n_t - lag_max - 1 - window, n_x, n_y])
+    c_max = np.zeros([n_t - lag_max - 1 - window, n_x, n_y])
+    
+    for s in range(lag_max + 1):
+        c_s = np.abs(cov_2pt(x, f, s, window)[lag_max + 1 + window - s:,:,:])
+
+        c += c_s
+        c2 += c_s * c_s
+        c_max = np.maximum(c_max, c)
+
+    c /= (lag_max + 1)
+    c2 = c2 / (lag_max + 1) - c * c
+
+    return (c_max - c) / c2
+
+
+def corr_2pt(x: npt.ArrayLike, f: npt.ArrayLike, lag: int, window: int):
     
     x = x[lag:, np.newaxis, np.newaxis]
 
-    # Edge case: 
+    # Edge case: lag == 0 then we can't and don't need to index by -lag 
     if lag != 0:
         f = f[:-lag, :, :]
     
@@ -38,6 +59,23 @@ def corr_2pt(x: npt.ArrayLike, f: npt.ArrayLike, lag: int, lag_max: int, window:
     c /= bn.move_std(a=x, window=window, axis=0) \
        * bn.move_std(a=f, window=window, axis=0)
 
+    return c
+
+
+def cov_2pt(x: npt.ArrayLike, f: npt.ArrayLike, lag: int, window: int):
+    
+    x = x[lag:, np.newaxis, np.newaxis]
+
+    # Edge case: lag == 0 then we can't and don't need to index by -lag 
+    if lag != 0:
+        f = f[:-lag, :, :]
+    
+    c = bn.move_mean(a=x*f, window=window, axis=0)
+    # print("Here")
+
+    c -= bn.move_mean(a=x, window=window, axis=0) \
+       * bn.move_mean(a=f, window=window, axis=0)
+    
     return c
 
 
@@ -54,6 +92,7 @@ def add_edges(edges: list[set], source: tuple[int, int],
             continue
         
         edges[t].add((source, (x, y)))
+        print(f"added {t}, {source}, {(x, y)}")
 
 
 def build_network(f: npt.ArrayLike, lag_max: int, thres: float, window: int = 365):
@@ -70,3 +109,19 @@ def build_network(f: npt.ArrayLike, lag_max: int, thres: float, window: int = 36
             add_edges(edges, (i, j), cmat, thres)
     
     return edges
+
+
+def build_network2(f: npt.ArrayLike, lag_max: int, window: int = 365):
+
+    n_t, n_x, n_y = f.shape
+
+    s = np.zeros(n_t - lag_max - 1 - window)
+
+    for i in range(n_x):
+        for j in range(n_y):
+            
+            print("Working on", i, j)
+            sij = str_2pt(f[:, i, j], f, lag_max, window)
+            s += np.mean(sij, axis=(1, 2))
+    
+    return s

test_2.py
@@ -2,7 +2,7 @@ import numpy as np
 import pickle
 
 from netCDF4 import Dataset
-from enso.network import build_network
+from enso.network import build_network2
 
 
 fname = "./data/t2m/2m_temp_corrected.nc"
@@ -19,8 +19,8 @@ mean = np.tile(mean, (43, 1, 1))
 t2m -= mean
 
 
-edges = build_network(t2m, 20, 0.5)
+s = build_network2(t2m, 200)
 
 
-with open("sample2.pkl", "wb") as f:
-    pickle.dump(edges, f)
+with open("sample3.pkl", "wb") as f:
+    pickle.dump(s, f)